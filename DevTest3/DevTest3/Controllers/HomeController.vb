445a9c6a8d2d506305144f754eae6b9626c624da
DESKTOP-CJ76F6P\khana <khanal.raj124@gmail.com>
2019-04-08 12:40:38 -0500

    al Commit for the reason why the search was not always working
    In HomeController.vb -> GetPlayerRanking method, I have replaced where clause with single and also changed the case to lower to avoid case sensitive search.
    Added conditions to set empty values in final result when no values are entered in search box.
    If any exception comes, we are displaying the message "Player not found"
    Used the ranking value 999999 to meet the condition specified in view where rank should be greater than zero.

---
+++ DevTest3/DevTest3/Controllers/HomeController.vb
@@ -0,0 +1,52 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+
+        Return View()
+
+    End Function
+
+    Function GetPlayerRanking() As ActionResult
+
+        Dim model As New GetPlayerRanking()
+        Return View(model)
+
+    End Function
+
+    <HttpPost>
+    Public Function GetPlayerRanking(model As GetPlayerRanking) As ActionResult
+        Try
+
+
+            Dim data As List(Of Player) = Player.CreatePlayersList()
+            Dim result As Player = Nothing
+
+            If Not String.IsNullOrEmpty(model.Name) Then
+                'result = data.Where(Function(d) d.Name.Contains(model.Name)).FirstOrDefault()
+
+                ' Replaced where with Single to fix the search. Changed the value to lower case to prevent case sensitive search.
+                result = data.Single(Function(x) x.Name.ToLower().Contains(model.Name.ToLower()))
+
+            End If
+
+            If result Is Nothing Then
+                model.Name = ""
+            Else
+                model.Name = result.Name
+            End If
+
+            If result Is Nothing Then
+                model.Ranking = 9999999
+            Else
+                model.Ranking = result.Ranking
+            End If
+        Catch ex As Exception
+            model.Ranking = 9999999
+            model.Name = "Player name not found"
+            Return View(model)
+        End Try
+        Return View(model)
+
+    End Function
+End Class
